c1d211c88e218cb0b581db90c12425ea55dd011c
Juan Carlos Trimiño <juancarlos@trimino.com>
2025-09-23 22:48:42 -0500

    clean the file.

--- MwCorrelationId.go
+++ MwCorrelationId.go
@@ -2,26 +2,20 @@ package middlewares
 
 import (
   "context"
-  "fmt"
   //The option -u instructs 'get' to update the module with dependencies.
   //go get -u github.com/google/uuid
   "github.com/google/uuid"
-  "github.com/juan-carlos-trimino/gplogger"
   "net/http"
   "time"
 )
 
 func CorrelationId(handler http.HandlerFunc) http.HandlerFunc {
   return func(res http.ResponseWriter, req *http.Request) {
-    start := time.Now()
     uuid := uuid.New()
     //Creating a new context from a parent context.
     ctx := context.WithValue(req.Context(), correlationIdKey, uuid.String())
     ctx = context.WithValue(ctx, startTimeKey, time.Now())
-    logger.LogInfo("Issuing correlationId to new request.", fmt.Sprintf("%s", uuid))
     //Calling the handler with the new context.
     handler.ServeHTTP(res, req.WithContext(ctx))
-    fmt.Printf("xxxxxRequest took %vms\nRequest correlation id: %s\n",
-      time.Since(start).Microseconds(), uuid)
   }
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
+//Uninstalling a library (or module) from a Go project primarily involves managing the go.mod file
+//and the module cache.
+//(1) Remove the Dependency from go.mod
+//    The first step is to remove the line corresponding to the library you want to uninstall from
+//    your project's go.mod file. This file lists all the direct dependencies of your module.
+//(2) Run go mod tidy
+//    After removing the dependency from go.mod, execute the following command in your terminal
+//    within your project's root directory:
+//    $ go mod tidy
+//
+//The default name for the generated executable would be:
 module github.com/juan-carlos-trimino/go-middlewares
 
 go 1.24.3
@@ -8,7 +19,6 @@ require (
 )
 
 require (
-	github.com/juan-carlos-trimino/gplogger v1.0.7 // indirect
 	github.com/juan-carlos-trimino/gposu v1.0.1 // indirect
 	golang.org/x/crypto v0.38.0 // indirect
 )
