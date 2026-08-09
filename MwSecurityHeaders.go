8c7c32e8fb968dcce9acf73828620248c04f9e27
Juan Carlos Trimiño <juancarlos@trimino.com>
2026-08-08 23:54:12 -0500

    header method: switch from .Add() to .Set().

--- MwSecurityHeaders.go
+++ MwSecurityHeaders.go
@@ -19,6 +19,7 @@ func SecurityHeaders(handler http.HandlerFunc) http.HandlerFunc {
     /***
     You must manipulate the headers before you execute the inner handler.
     SET SECURITY HEADERS FIRST (Before running the handler)
+    Switching from .Add() to .Set() overrides previous defaults cleanly instead of appending values onto an array slice.
     ***/
     /***
     In order to make the server more secure, it is recommended that security headers be added to each response.
@@ -29,7 +30,7 @@ func SecurityHeaders(handler http.HandlerFunc) http.HandlerFunc {
     https://blog.appcanary.com/2017/http-security-headers.html#hsts.
     max-age = 365 days.
     ***/
-    res.Header().Add("Strict-Transport-Security", "max-age=31536000; includeSubdomains; preload")
+    res.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubdomains; preload")
     /***
     For more information about Content Security Policy (HTTP Security Headers), see
     https://blog.appcanary.com/2017/http-security-headers.html
@@ -43,7 +44,7 @@ func SecurityHeaders(handler http.HandlerFunc) http.HandlerFunc {
     font-src 'self' https://fonts.gstatic.com: It allows your page to download the actual binary font files (woff2).
     Safety: It is safe from a security standpoint because it restricts external code execution.
     ***/
-    res.Header().Add("Content-Security-Policy", "default-src 'none'; script-src 'self'; style-src 'self' https://fonts.googleapis.com; " +
+    res.Header().Set("Content-Security-Policy", "default-src 'none'; script-src 'self'; style-src 'self' https://fonts.googleapis.com; " +
       "connect-src 'self'; img-src 'self'; frame-src 'self'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; " +
       "font-src 'self' https://fonts.gstatic.com;")
     /***
@@ -51,20 +52,20 @@ func SecurityHeaders(handler http.HandlerFunc) http.HandlerFunc {
     Its use can introduce XSS vulnerabilities in otherwise safe websites. This should not be used unless you need to support older web browsers
     that don't yet support CSP. For an explanation, see https://blog.appcanary.com/2017/http-security-headers.html#x-xss-protection
     ***/
-    res.Header().Add("X-XSS-Protection", "1; mode=block")
+    res.Header().Set("X-XSS-Protection", "1; mode=block")
     /***
     X-Frame-Options is an HTTP header that allows sites control over how your site may be framed within an iframe. Clickjacking is a practical
     attack that allows malicious sites to trick users into clicking links on your site even though they may appear to not be on your site at
     all. For an explanation, see https://blog.appcanary.com/2017/http-security-headers.html#x-frame-options
     ***/
-    res.Header().Add("X-Frame-Options", "DENY")
+    res.Header().Set("X-Frame-Options", "DENY")
     /***
     The Referrer-Policy HTTP header governs which referrer information, sent in the Referer header, should be included with requests made. The
     "strict-origin-when-cross-origin" sends a full URL when performing a same-origin request, only sends the origin of the document to a-priori
     as-much-secure destination (HTTPS->HTTPS), and sends no header to a less secure destination (HTTPS->HTTP). For an explanation, see
     https://blog.appcanary.com/2017/http-security-headers.html#referrer-policy
     ***/
-    res.Header().Add("Referrer-Policy", "strict-origin-when-cross-origin")
+    res.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
     /***
     Setting this header will prevent the browser from interpreting files as something else than declared by the content type in the HTTP headers.
     Without this header, browsers can incorrectly detect files as scripts and stylesheets, leading to XSS attacks. For an explanation, see
@@ -74,7 +75,7 @@ func SecurityHeaders(handler http.HandlerFunc) http.HandlerFunc {
     * WARNING: If this header is set to 'nosniff', you MUST ENSURE the 'Content-Type' header is set correctly. *
     ************************************************************************************************************
     ***/
-    res.Header().Add("X-Content-Type-Options", "nosniff")
+    res.Header().Set("X-Content-Type-Options", "nosniff")
     /***
     In responses, a Content-Type header provides the client with the actual content type of the returned content. This header's value may be
     ignored, for example when browsers perform MIME sniffing; set the X-Content-Type-Options header value to nosniff to prevent this behavior.
